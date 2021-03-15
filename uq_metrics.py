6a10bb0a65c5069612b8bb8b19bbbb348aaf7b1b
Suraj Yerramilli <surajyerramilli@gmail.com>
2021-03-15 08:03:12 -0500

    major corrections

--- uq_metrics.py
+++ uq_metrics.py
@@ -5,7 +5,7 @@ def neg_interval_score(y_true,lq,uq,alpha):
     term1 = uq-lq
     term2 = 2/alpha*torch.nn.functional.relu(lq-y_true)
     term3 = 2/alpha*torch.nn.functional.relu(-uq+y_true)
-    return (term1+term2+term3).mean()
+    return (term1+term2+term3).mean().item()
 
 def neg_crps_gaussian(y_true,mean,std):
     z = (y_true-mean)/std
@@ -17,7 +17,7 @@ def neg_crps_gaussian(y_true,mean,std):
 
 def metrics_ensemble(y_true,y_means,y_stds,alpha=0.05):
     # %% prediction metric
-    rmse =((y_true-y_means.mean(axis=0)**2).mean().sqrt()
+    rmse =((y_true-y_means.mean(axis=0))**2).mean().sqrt()
     
     means= y_means.t()
     stds = y_stds.t()
@@ -26,17 +26,17 @@ def metrics_ensemble(y_true,y_means,y_stds,alpha=0.05):
     num_samples = 1000
     I = torch.randint(high=means.shape[1],size=(means.shape[0],num_samples)).long()
     samples = torch.stack([
-        means[i,I[i,0]]+stds[i,I[i,1]]*torch.randn((num_samples,)) for i in range(len(y_test))
+        means[i,I[i,:]]+stds[i,I[i,:]]*torch.randn((num_samples,)) for i in range(len(y_true))
     ],axis=0)
     
     # coverage and interval-score 
     lq = samples.quantile(alpha/2,axis=-1)
     uq = samples.quantile(1-alpha/2,axis=-1)
-    cov_prob = (1.*(y_test>=lq)*(y_test<=uq)).mean()
+    cov_prob = (1.*(y_true>=lq)*(y_true<=uq)).mean()
     mean_is = neg_interval_score(y_true,lq,uq,alpha)
     
     # crps
-    term1 = (samples-y_test.unsqueeze(-1)).abs().mean(axis=1)
+    term1 = (samples-y_true.unsqueeze(-1)).abs().mean(axis=1)
     forecasts_diff = torch.abs(samples.unsqueeze(-1)-samples.unsqueeze(-2))
     term2 = 0.5*forecasts_diff.mean(axis=(-2,-1))
     crps = (term1-term2).mean()
