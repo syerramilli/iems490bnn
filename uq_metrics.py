e702c21abe1ca8bb8fd993989124a912f9af59d7
Suraj Yerramilli <surajyerramilli@gmail.com>
2021-03-16 07:38:47 -0500

    converting vectorized operations to for loop for memory reasons

--- uq_metrics.py
+++ uq_metrics.py
@@ -23,27 +23,38 @@ def metrics_ensemble(y_true,y_means,y_stds,alpha=0.05):
     stds = y_stds.t()
     
     # number of samples-1000
+    n_obs = len(y_true)
     num_samples = 1000
-    I = torch.randint(high=means.shape[1],size=(means.shape[0],num_samples)).long()
-    samples = torch.stack([
-        means[i,I[i,:]]+stds[i,I[i,:]]*torch.randn((num_samples,)) for i in range(len(y_true))
-    ],axis=0)
-    
-    # coverage and interval-score 
-    lq = samples.quantile(alpha/2,axis=-1)
-    uq = samples.quantile(1-alpha/2,axis=-1)
-    cov_prob = (1.*(y_true>=lq)*(y_true<=uq)).mean().item()
-    mean_is = neg_interval_score(y_true,lq,uq,alpha)
-    
-    # crps
-    term1 = (samples-y_true.unsqueeze(-1)).abs().mean(axis=1)
-    forecasts_diff = torch.abs(samples.unsqueeze(-1)-samples.unsqueeze(-2))
-    term2 = 0.5*forecasts_diff.mean(axis=(-2,-1))
-    crps = (term1-term2).mean().item()
+    lq = []
+    uq = []
+    interval_score = 0
+    cov = 0
+    crps = 0
+
+    for i in range(n_obs):
+        I = torch.randint(high=means.shape[1],size=(num_samples,))
+        samples = means[i,I]+stds[i,I]*torch.randn((num_samples,))
+        lq = samples.quantile(alpha/2)
+        uq = samples.quantile(1-alpha/2)
+        
+        # coverage 
+        cov += (1*(y_true[i]>=lq)*(y_true[i]<=uq)).item()
+
+        # interval score
+        term1 = uq-lq
+        term2 = 2/alpha*torch.nn.functional.relu(lq-y_true[i])
+        term3 = 2/alpha*torch.nn.functional.relu(-uq+y_true[i])
+        interval_score += (term1+term2 + term3).item()
+
+        # crps
+        term1 = (samples-y_true[i]).abs().mean()
+        forecasts_diff = torch.abs(samples.unsqueeze(-1)-samples.unsqueeze(-2))
+        term2 = 0.5*forecasts_diff.mean()
+        crps += (term1-term2).item()
     
     return {
         'rmse':rmse,
-        'mean_is':mean_is,
-        'coverage_prob':cov_prob,
-        'crps':crps
+        'mean_is':interval_score/n_obs,
+        'coverage_prob':cov/n_obs,
+        'crps':crps/n_obs
     }
